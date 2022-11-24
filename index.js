5d825e79e59d8cd7f8419bec3360675dc69fc298
gkundan <beingkundan188@gmail.com>
2022-11-24 22:15:32 +0530

    set the view engine

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 8000;
 ///****     --- used express router         */
 app.use("/", require("./routes"));
 
+///****     --- Views Engine..     */
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 //******    -- FireUp the Server >     */
 app.listen(port, (err) => {
   if (err) {
