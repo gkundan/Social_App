d1f65a30b51d6d0fd10edece6e7b8cc1cbdbbe43
gkundan <beingkundan188@gmail.com>
2022-12-01 08:15:45 +0530

    : add the ejs and create a view engine

--- controller/home_contoller.js
+++ controller/home_contoller.js
@@ -1,4 +1,6 @@
 //create a controller and send on route ans use as request route..
 module.exports.home = function (req, res) {
-  return res.end("<h1>Hello from  Homie ...</h1>");
+  return res.render("home", {
+    title: "Home",
+  });
 };
